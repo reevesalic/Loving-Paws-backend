b4ac65ceea3e763bf1953ddc8a3195d799551537
reevesalic <reevesalic@gmail.com>
2021-04-11 14:17:22 -0400

    added migration and seed data

--- db/migrate/20210410220221_create_animals.rb
+++ db/migrate/20210410220221_create_animals.rb
@@ -1,7 +1,7 @@
 class CreateAnimals < ActiveRecord::Migration[6.0]
   def change
     create_table :animals do |t|
-      t.string :type
+      t.string :animal
       t.string :breed
       t.string :location
       t.integer :age
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+lisa = Contact.create(name: "Lisa", email: "lisa@gmail.com", phone: 1234567890)
+mike = Contact.create(name: "Mike", email: "mike@gmail.com", phone: 1234567890)
+kelly = Contact.create(name: "Kelly", email: "kelly@gmail.com", phone: 1234567890)
+
+animal1 = Animal.create(animal: "cat", breed: "long hair", location: "PA", age: 9, description: "9 year old male up for adoption", history: "n/a", image_url: "https://www.rd.com/wp-content/uploads/2019/04/shutterstock_1013848126.jpg?resize=2048,1365", contact_id: lisa.id)
+
+
+      
