5364c2c463afb2cd9a87c7127f65767438d36827
reevesalic <reevesalic@gmail.com>
2021-04-13 15:22:46 -0400

    set up routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :animals
-  resources :contacts
-  resources :names
+  namespace :api do
+    namespace :v1 do
+      resources :animals
+      resources :contacts
+      
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+end
 end
