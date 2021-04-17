1dcb5e4d9f6c4e2fa3cc05a36b9e3bcbd628110e
reevesalic <reevesalic@gmail.com>
2021-04-17 10:20:33 -0400

    add cors to the config/application file

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module LovingPawsBackend
@@ -33,5 +34,15 @@ module LovingPawsBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+      origins ‘*’
+      resource ‘*’, :headers => :any, :methods => [:get,
+                                                   :post,
+                                                   :patch,
+                                                   :delete,
+                                                   :options]
+    end
+    end
   end
 end
